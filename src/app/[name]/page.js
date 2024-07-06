b21fdaa2213473f470420cf1d4118c5ba4acdf70
Shady Idrissi <chadiidac@gmail.com>
2024-07-06 03:04:36 +0100

    edit page view iteams

---
+++ src/app/[name]/page.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../pages.style.css'
+
+function page({props}) {
+  return (
+    <div className='div-iteam'>
+        <h1>hello wolrd</h1>
+        <p>paragraph paragraph paragraph paragraph paragraph paragraph paragraph paragraph paragraph paragraph paragraph paragraph paragraph paragraph paragraph paragraph paragraph paragraph paragraph paragraph</p>
+        <div id='flag'>
+            <p>city</p>
+            <img src="" alt="iamge" />
+        </div>
+        <div className='map'></div>
+        <div className='div-list-details'>
+            <h3>hello world</h3>
+            <ul>
+                <li>c</li>
+                <li>c</li>
+                <li>c</li>
+                <li>c</li>
+                <li>c</li>
+            </ul>
+        </div>
+        <p>paragraph</p>
+        
+    </div>
+  )
+}
+
+export default page
